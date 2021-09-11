30437d5d63f9af69cc864f285809faf43b03a482
Catherine V <32275945+Catherine1998@users.noreply.github.com>
2021-09-10 23:51:54 -0600

    Busqueda de producto

--- Ecommerce-proyect/Ecommerce-proyect/Controllers/CatalogoProductosController.cs
+++ Ecommerce-proyect/Ecommerce-proyect/Controllers/CatalogoProductosController.cs
@@ -37,11 +37,11 @@ namespace Ecommerce_proyect.Controllers
                 {
                     catalogoProductosViews.Add(new CatalogoProductosView()
                     {
-                        Id = catalogo.Id,
-                        Nombre = catalogo.Nombre,
-                        Descripcion = catalogo.Descripcion,
-                        PrecioConIva = catalogo.PrecioConIva,
-                        LinkImagen = catalogo.Imagen
+                        Id            = catalogo.Id,
+                        Nombre        = catalogo.Nombre,
+                        Descripcion   = catalogo.Descripcion,
+                        PrecioConIva  = catalogo.PrecioConIva,
+                        LinkImagen    = catalogo.Imagen
                     });
                 });
 
@@ -50,6 +50,47 @@ namespace Ecommerce_proyect.Controllers
             catch (Exception ex)
             {
                 return StatusCode((int)HttpStatusCode.InternalServerError, new { mensaje = ex });
+            }
+        }
+
+
+
+        [HttpGet("{search}")]
+        public IActionResult SearchProductos([FromRoute] string search)
+        {
+            try
+            {
+                var catalogoBusqueda = context.Productos.Include(producto => producto.Categorias).Where(producto => producto.Categorias.Marca == search && producto.Estado == 1 || producto.Categorias.Tipo == search && producto.Estado == 1 || producto.Categorias.Modelo == search && producto.Estado == 1).ToList();
+
+                List<CatalogoProductosView> catalogoProductosViews = new List<CatalogoProductosView>();
+
+               if( catalogoBusqueda.Count != 0)
+                {
+                    catalogoBusqueda.ForEach(productoBusqueda =>
+                    {
+                        catalogoProductosViews.Add(new CatalogoProductosView()
+                        {
+                            Id              = productoBusqueda.Id,
+                            Nombre          = productoBusqueda.Nombre,
+                            Descripcion     = productoBusqueda.Descripcion,
+                            PrecioConIva    = productoBusqueda.PrecioConIva,
+                            LinkImagen      = productoBusqueda.Imagen
+                        });
+
+                    });
+
+                    return StatusCode((int)HttpStatusCode.OK, catalogoProductosViews);
+                }
+                else
+                {
+                    return StatusCode((int)HttpStatusCode.NotFound, new { mensaje = "El producto no fue encontrado" });
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, new { mensaje = ex.Message });
             }
         }
     }
