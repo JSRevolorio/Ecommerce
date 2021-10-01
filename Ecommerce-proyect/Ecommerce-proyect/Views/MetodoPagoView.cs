d2a7d6efa7f4a0566a1a05ef8f3e7d65f1cde52c
Catherine V <32275945+Catherine1998@users.noreply.github.com>
2021-09-30 22:32:40 -0600

    CRUD metodo pago

---
+++ Ecommerce-proyect/Ecommerce-proyect/Views/MetodoPagoView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce_proyect.Views
+{
+    public class MetodoPagoView
+    {
+
+       
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(50)]
+        public string Tipo { get; set; }
+
+
+
+
+    }
+}
