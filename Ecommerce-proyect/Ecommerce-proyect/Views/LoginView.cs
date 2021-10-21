eaa7d241777054e846b59e147b4fefaf65dce6df
Catherine V <32275945+Catherine1998@users.noreply.github.com>
2021-10-21 15:22:46 -0600

    CRUD y login usuario

---
+++ Ecommerce-proyect/Ecommerce-proyect/Views/LoginView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace Ecommerce_proyect.Views
+{
+    public class LoginView
+    {
+
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Usuario { get; set; }
+
+        [Required]
+        public string Contraseña { get; set; }
+
+    }
+}
---
+++ Ecommerce-proyect/Ecommerce-proyect/Views/UsuarioView.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce_proyect.Views
+{
+    public class UsuarioView
+    {
+        public int Id { get; set; }
+        public int? IdEmpleadoRol { get; set; }
+
+        public string RolEmpleado { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Usuario1 { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Contraseña { get; set; }
+
+    }
+}
