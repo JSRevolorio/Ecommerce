b7516291b911acb6613bd03cf87f260b9eb71193
JSRevolorio <jrevoloriod@gmail.com>
2021-10-23 08:32:56 -0600

    Login

---
+++ Ecommerce-proyect/Ecommerce-proyect/Views/LoginView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce_proyect.Views
+{
+    public class LoginView
+    {
+        public string Usuario { get; set; }
+        public string Contraseña { get; set; }
+    }
+}
